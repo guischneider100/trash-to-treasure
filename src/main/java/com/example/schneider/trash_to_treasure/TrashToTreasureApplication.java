60d4f6733793c5047316bb813f94374ef3b6c417
guischneider100 <guilhermeschneider@unochapeco.edu.br>
2025-05-21 14:21:36 +1000

    Initial commit - Trash to Treasure

---
+++ src/main/java/com/example/schneider/trash_to_treasure/TrashToTreasureApplication.java
@@ -0,0 +1,13 @@
+package com.example.schneider.trash_to_treasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashToTreasureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrashToTreasureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/schneider/trash_to_treasure/TrashToTreasureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schneider.trash_to_treasure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashToTreasureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
